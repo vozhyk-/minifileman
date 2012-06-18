026f73f31a4567178a7f41ea33b2dbcfcf605877
Vitaut Bajaryn <vitaut.bayaryn@gmail.com>
2012-06-19 02:23:00 +0300

    fs-list-directory-add-:resolve-symlinks
    Ignore-this: d6e8e5e2ffddb46aecd19449ea129689
    
    darcs-hash:20120618232300-c6c69-c92ebfb2505ef3f78a4bdab214e61e7e253a2e3c

--- src/filesystem.lisp
+++ src/filesystem.lisp
@@ -1,5 +1,40 @@
 (in-package #:minifileman.filesystem)
 
+;;; Redefine cl-fad:list-directory (add :resolve-symlinks option)
+(defun list-directory (dirname &rest args &key (resolve-symlinks t))
+  "Returns a fresh list of pathnames corresponding to the truenames of
+all files within the directory named by the non-wild pathname
+designator DIRNAME.  The pathnames of sub-directories are returned in
+directory form - see PATHNAME-AS-DIRECTORY."
+  (declare (ignorable args resolve-symlinks))
+  (when (wild-pathname-p dirname)
+    (error "Can only list concrete directory names."))
+  #+:ecl
+  (let ((dir (pathname-as-directory dirname)))
+    (concatenate 'list
+                 (apply #'directory (merge-pathnames (pathname "*/") dir args))
+                 (apply #'directory (merge-pathnames (pathname "*.*") dir args))))
+  #-:ecl
+  (let ((wildcard (cl-fad::directory-wildcard dirname)))
+    #+:abcl (system::list-directory dirname resolve-symlinks)
+    #+:sbcl (apply #'directory wildcard args)
+    #+(or :cmu :scl) (directory wildcard :truenamep resolve-symlinks)
+    #+:lispworks (directory wildcard :link-transparency resolve-symlinks)
+    #+(or :openmcl :digitool)
+    (directory wildcard :directories t :follow-links resolve-symlinks)
+    ;; No resolve-symlinks (no such option)
+    #+:allegro (directory wildcard :directories-are-files nil)
+    #+:clisp
+    (let ((list (nconc (directory wildcard :if-does-not-exist :keep :full t)
+                       (directory (cl-fad::clisp-subdirectories-wildcard wildcard)
+                                  :full t))))
+      (mapcar (if resolve-symlinks #'second #'first) list))
+    ;; No resolve-symlinks (couldn't find Corman Lisp's documentation)
+    #+:cormanlisp (nconc (directory wildcard)
+                         (cl::directory-subdirs dirname)))
+  #-(or :sbcl :cmu :scl :lispworks :openmcl :allegro :clisp :cormanlisp :ecl :abcl :digitool)
+  (error "LIST-DIRECTORY not implemented"))
+
 (defun list-directory-relative (dirname)
   (mapcar #'basename
           (list-directory dirname)))
