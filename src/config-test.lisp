c33126ed170d12788192e9ab3eb1f5a30ddb431f
Vitaut Bajaryn <vitaut.bayaryn@gmail.com>
2012-06-24 13:44:45 +0300

    config-test-update
    Ignore-this: 4f3ed205959bf63196bab86d2dc990a8
    
    darcs-hash:20120624104445-c6c69-850438ef253a07839b2a6a7b0d7fdec0949eb7a0

--- src/config-test.lisp
+++ src/config-test.lisp
@@ -1,5 +1,5 @@
-(in-package #:cl-user)
 (load "load.lisp")
+(in-package #:minifileman.config)
 (define-config
   (("default_dir:use" :default t
 		      :depends-on
@@ -7,4 +7,3 @@
    ("default_dir" :default "/"))
   (:default-path #P"/home/vozhyk/.minifileman/minifileman.conf"))
 (defparameter *config* (make-instance 'config))
-(clear-config)
