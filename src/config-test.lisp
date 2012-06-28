e6c1918dca257a3762d2e350cec0a27cd9898cac
Vitaut Bajaryn <vitaut.bayaryn@gmail.com>
2012-06-28 18:45:24 +0300

    config-test-vars-replace-_->-
    Ignore-this: 3433996784787abad48c03b4f333bd67
    
    darcs-hash:20120628154524-c6c69-5e78cc931872f3f6b1271f9786e5fc56d9b631c5

--- src/config-test.lisp
+++ src/config-test.lisp
@@ -1,10 +1,10 @@
 (load "load.lisp")
 (in-package #:minifileman.config)
 (define-config
-  (("default_dir:use" :default t
+  (("default-dir:use" :default t
 		      :depends-on
                       #'(lambda (value)
                           (when (eql value t) "default_dir")))
-   ("default_dir" :default "/"))
+   ("default-dir" :default "/"))
   (:default-path #P"/home/vozhyk/.minifileman/minifileman.conf"))
 (defparameter *config* (make-instance 'config))
